6e81b3f227fc0ea3d4a94575680f3518084ef632
SinInchisama <sun98390123@gmail.com>
2024-05-22 13:53:51 +0900

    검색창 함수만드는중
    MainGUI 클래스로 만들어두고
    산검색, 지역검색에 대한 변수를 만들었음.

--- UI배치테스트.py
+++ UI배치테스트.py
@@ -2,103 +2,119 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter.scrolledtext import ScrolledText
 
-Window = Tk()
-Window.title("등산가는길")
-Window.geometry("800x800")
+class MainGUI:
+    def Search_Area(self):
+        self.SelectM = self.Txt1.get()
 
-# Frame1 검색창에 대한 프레임
+    def Search_Moutain(self):
+        self.SelectA = self.Txt2.get()
+        self.SelectM = None
 
-Note = Notebook(Window)
-Note.place(x=10, y=10, width=780, height=780)
+    def __init__(self):
+        self.SelectM = None     # 마운틴 검색을 저장하는 변수
+        self.SelectA = None     # 지역 검색을 저장하는 변수
+        self.initWindow()       # tkinter 윈도우를 초기화
 
-Frame1 = Frame(Note, width=780, height=780)
-Note.add(Frame1,text = " 검색 ")
+    def initWindow(self):
+        self.Window = Tk()
+        self.Window.title("등산가는길")
+        self.Window.geometry("800x800")
 
-# 지역 검색창 그리기
-Search_Area_Text = Label(Frame1, text=" 지역 검색창(광역시나 자치도를 입력해주세요) ")
-Search_Area_Text.place(x=20,y=0)
+        # Frame1 검색창에 대한 프레임
 
-Txt1 = Entry(Frame1)
-Txt1.place(x=10,y=20, width=280, height=30)
+        self.Note = Notebook(self.Window)
+        self.Note.place(x=10, y=10, width=780, height=780)
 
-Search_Area_Button = Button(Frame1,text = " 검색 ")
-Search_Area_Button.place(x=300,y=22)
+        self.Frame1 = Frame(self.Note, width=780, height=780)
+        self.Note.add(self.Frame1,text = " 검색 ")
 
+        # 지역 검색창 그리기
+        Search_Area_Text = Label(self.Frame1, text=" 지역 검색창(광역시나 자치도를 입력해주세요) ")
+        Search_Area_Text.place(x=20,y=0)
 
-# 산 검색창 그리기
-Guide_Moutain = Label(Frame1, text=" 산 검색창 ")
-Guide_Moutain.place(x=20,y=50)
+        self.Txt1 = Entry(self.Frame1)
+        self.Txt1.place(x=10,y=20, width=280, height=30)
 
-Txt2 = Entry(Frame1,width= 40)
-Txt2.place(x=10,y=70, width=280, height=30)
-
-Search_Area_Button = Button(Frame1,text = " 검색 ")
-Search_Area_Button.place(x=300,y=72)
-
-
-# 산 목록을 출력하는 곳
-Scrolled_Moutain = ScrolledText(Frame1,width=20,height=40)
-Scrolled_Moutain.place(x= 10 ,y=120)
-
-Select_Mountain= Button(Frame1,text = " 선택 ")
-Select_Mountain.place(x=10,y=660,width=150, height=80)
+        self.Search_Area_Button = Button(self.Frame1,text = " 검색 ",command=self.Search_Area)
+        self.Search_Area_Button.place(x=300,y=22)
 
 
-# 산불위험도를 출력하는 곳
-Fires_Danger_Canvas = Canvas(Frame1, width=220, height=220,bg = "red")
-Fires_Danger_Canvas.place(x=170,y=120)
+        # 산 검색창 그리기
+        Guide_Moutain = Label(self.Frame1, text=" 산 검색창 ")
+        Guide_Moutain.place(x=20,y=50)
+
+        self.Txt2 = Entry(self.Frame1,width= 40)
+        self.Txt2.place(x=10,y=70, width=280, height=30)
+
+        self.Search_Moutain_Button = Button(self.Frame1,text = " 검색 ",command=self.Search_Moutain)
+        self.Search_Moutain_Button.place(x=300,y=72)
 
 
-# 해발고도 그래프를 출력하는 곳
-Altitude_Canvas = Canvas(Frame1, width=590, height=386,bg = "yellow")
-Altitude_Canvas.place(x=170,y=350)
+        # 산 목록을 출력하는 곳
+        self.Scrolled_Moutain = ScrolledText(self.Frame1,width=20,height=40)
+        self.Scrolled_Moutain.place(x= 10 ,y=120)
+
+        self.Select_Mountain= Button(self.Frame1,text = " 선택 ")
+        self.Select_Mountain.place(x=10,y=660,width=150, height=80)
 
 
-# 지도를 출력하는 곳
-Map_Canvas = Canvas(Frame1, width=360, height=320,bg = "blue")
-Map_Canvas.place(x=400,y= 20)
+        # 산불위험도를 출력하는 곳
+        self.Fires_Danger_Canvas = Canvas(self.Frame1, width=220, height=220,bg = "red")
+        self.Fires_Danger_Canvas.place(x=170,y=120)
+
+
+        # 해발고도 그래프를 출력하는 곳
+        self.Altitude_Canvas = Canvas(self.Frame1, width=590, height=386,bg = "yellow")
+        self.Altitude_Canvas.place(x=170,y=350)
+
+
+        # 지도를 출력하는 곳
+        self.Map_Canvas = Canvas(self.Frame1, width=360, height=320,bg = "blue")
+        self.Map_Canvas.place(x=400,y= 20)
 
 
 
 
-# Frame2 세부정보창에 대한 프레임
+        # Frame2 세부정보창에 대한 프레임
 
-Frame2 = Frame(Note, width=780, height=780)
-Note.add(Frame2,text = " 정보 ")
+        self.Frame2 = Frame(self.Note, width=780, height=780)
+        self.Note.add(self.Frame2,text = " 정보 ")
 
-# 선택한 산의 교통시설을 보여주는 스크롤바
+        # 선택한 산의 교통시설을 보여주는 스크롤바
 
-Scrolled_Transport = ScrolledText(Frame2,width=40,height=25)
-Scrolled_Transport.place(x= 10 ,y=20)
-
-
-# 교통 시설 선택 버튼
-Select_Transport_Button = Button(Frame2,text = " 선택 ")
-Select_Transport_Button.place(x=350,y=40,width=100, height=80)
+        self.Scrolled_Transport = ScrolledText(self.Frame2,width=40,height=25)
+        self.Scrolled_Transport.place(x= 10 ,y=20)
 
 
-# 텔레그램 선택 버튼
-Select_Telegram_Button = Button(Frame2,text = " 텔레그램 ")
-Select_Telegram_Button.place(x=350,y=140,width=100, height=80)
+        # 교통 시설 선택 버튼
+        self.Select_Transport_Button = Button(self.Frame2,text = " 선택 ")
+        self.Select_Transport_Button.place(x=350,y=40,width=100, height=80)
 
 
-# 메일 선택 버튼
-Select_Telegram_Button = Button(Frame2,text = " 메일 ")
-Select_Telegram_Button.place(x=350,y=240,width=100, height=80)
+        # 텔레그램 선택 버튼
+        self.Select_Telegram_Button = Button(self.Frame2,text = " 텔레그램 ")
+        self.Select_Telegram_Button.place(x=350,y=140,width=100, height=80)
 
 
-# 교통시설의 지도를 그리는 캔버스
-Map_Canvas = Canvas(Frame2, width=265, height=320,bg = "blue")
-Map_Canvas.place(x=500,y= 20)
+        # 메일 선택 버튼
+        self.Select_Telegram_Button = Button(self.Frame2,text = " 메일 ")
+        self.Select_Telegram_Button.place(x=350,y=240,width=100, height=80)
 
 
-# 정보들을 출력하는 캔버스
-Data_Canvas = Canvas(Frame2, width=480, height=360,bg = "blue")
-Data_Canvas.place(x=10,y= 380)
+        # 교통시설의 지도를 그리는 캔버스
+        self.Map_Canvas = Canvas(self.Frame2, width=265, height=320,bg = "blue")
+        self.Map_Canvas.place(x=500,y= 20)
 
 
-# 산 이미지를 출력하는 캔버스
-Image_Canvas = Canvas(Frame2, width=265, height=360,bg = "yellow")
-Image_Canvas.place(x=500,y= 380)
+        # 정보들을 출력하는 캔버스
+        self.Data_Canvas = Canvas(self.Frame2, width=480, height=360,bg = "blue")
+        self.Data_Canvas.place(x=10,y= 380)
 
-Window.mainloop()
+
+        # 산 이미지를 출력하는 캔버스
+        self.Image_Canvas = Canvas(self.Frame2, width=265, height=360,bg = "yellow")
+        self.Image_Canvas.place(x=500,y= 380)
+
+        self.Window.mainloop()
+
+MainGUI()
