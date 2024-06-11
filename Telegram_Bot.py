662fa7f20c08bf37e1116dc1b28536fef4c7733a
Sinichisama <sun98390123@gmail.com>
2024-06-12 03:59:19 +0900

    텔레그램 봇 코드 구현
    테스트예정

--- Telegram_Bot.py
+++ Telegram_Bot.py
@@ -7,6 +7,20 @@ class Telegram_Bot:
         for key,value in Dict.items():
             self.Bot.sendMessage(chat_id= self.Chat_Id,text = value)
 
+    def replyAptData(self,user, loc_param):
+        # 이부분은 데이터를 받아오는 부분인데 나는 다르게 바꿔서 #res_list = noti.getData(loc_param, date_param)
+        res_list = self.getData(loc_param)
+        msg = ''
+        for r in res_list:
+            self.Pass_Message(r)
+
+    def getData(self,loc_param):
+        M_list = list()
+        for key,item in ReadXML.Mountain_Data.items():
+            if(item['위치'] == loc_param):
+                M_list.append({"산이름":key,'위치':item['위치'],'주소':item['주소'],'위도':item['위도'],'경도':item['경도'],'고도':item['고도']})
+        return M_list
+
     def Handle(self,msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
         if content_type != 'text':
@@ -17,12 +31,13 @@ class Telegram_Bot:
         args = text.split(' ')
 
         if args[0] in ReadXML.CtpvNm and args <2:
-
-
+            self.replyAptData(chat_id, args[1])
+        else:
+            self.Bot.sendMessage(chat_id, '모르는 명령어입니다.\n정확한 지역명을 알려주세요.')
 
 
     def __init__(self):
         Bot_Token = "7345270414:AAGeSMb4hen7L28HZR5dH3yzy6Y34b8raI4"
         self.Chat_Id = "7281344702"
         self.Bot = telepot.Bot(token=Bot_Token)
-        self.Bot.message_loop(Handle)
+        #self.Bot.message_loop(Handle)
